f8150f1d15693e098d57818f7515327d3d3bf6ed
Aurelio787 <aureliopini007@gmail.com>
2026-06-15 13:35:43 +0200

    jh

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,11 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeswitch = document.getElementById('theme-switch')
 
+const enabledarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'avtive')
+}
+const disabledarkmode
+themeswitch.addEventListener("click", () => {
+    darkmode !== "active"? enabledarkmode() : disabledarkmode() 
+})
